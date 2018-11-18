66c38157a1ead8dce908d36bb2b1d18c7148f8df
LifeH <hwie.zhu@gmail.com>
2018-11-18 16:14:47 -0500

    added booklist.js

---
+++ BookList.compiled.js
@@ -0,0 +1,233 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+    "use strict";
+    
+    /**
+     * Uses the react-transform babel plugin
+     * to rewrite modules so that all react components are
+     * exported.
+     *
+     * This allows us to access those components and test them.
+     *
+     * ex:
+     *
+     * // component.js
+     *
+     * var MyComponent = React.createClass({});
+     *
+     * // component-test.js
+     *
+     * var components = require('../component.js');
+     *
+     * console.log(components.__ReactComponents.MyComponent);
+     *
+     */
+    
+    module.exports = function createExport(_ref) {
+      var locals = _ref.locals;
+    
+    
+      return function (ReactClass, componentId) {
+    
+        if (!locals[0].exports) {
+          locals[0].exports = {};
+        }
+    
+        if (!locals[0].exports.__ReactComponents) {
+          locals[0].exports.__ReactComponents = [];
+        }
+    
+        locals[0].exports.__ReactComponents.push(ReactClass);
+    
+        return ReactClass;
+      };
+    };
+    
+    },{}],2:[function(require,module,exports){
+    'use strict';
+    
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.BestSeller = undefined;
+    
+    var _react2 = require('react');
+    
+    var _react3 = _interopRequireDefault(_react2);
+    
+    var _reactTransformComponentExports = require('/home/ccuser/.babelscripts/react-transform-component-exports');
+    
+    var _reactTransformComponentExports2 = _interopRequireDefault(_reactTransformComponentExports);
+    
+    var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+    
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    
+    function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+    
+    function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+    
+    var _components = {
+      BestSeller: {
+        displayName: 'BestSeller'
+      }
+    };
+    
+    var _homeCcuserBabelscriptsReactTransformComponentExports2 = (0, _reactTransformComponentExports2.default)({
+      filename: '/home/ccuser/workspace/react-102-proptypes-proptypes-apply/BestSeller.js',
+      components: _components,
+      locals: [module],
+      imports: [_react3.default]
+    });
+    
+    function _wrapComponent(id) {
+      return function (Component) {
+        return _homeCcuserBabelscriptsReactTransformComponentExports2(Component, id);
+      };
+    }
+    
+    var BestSeller = exports.BestSeller = _wrapComponent('BestSeller')(function (_React$Component) {
+      _inherits(BestSeller, _React$Component);
+    
+      function BestSeller() {
+        _classCallCheck(this, BestSeller);
+    
+        return _possibleConstructorReturn(this, (BestSeller.__proto__ || Object.getPrototypeOf(BestSeller)).apply(this, arguments));
+      }
+    
+      _createClass(BestSeller, [{
+        key: 'render',
+        value: function render() {
+          return _react3.default.createElement(
+            'li',
+            null,
+            'Title: ',
+            _react3.default.createElement(
+              'span',
+              null,
+              this.props.title
+            ),
+            _react3.default.createElement('br', null),
+            'Author: ',
+            _react3.default.createElement(
+              'span',
+              null,
+              this.props.author
+            ),
+            _react3.default.createElement('br', null),
+            'Weeks: ',
+            _react3.default.createElement(
+              'span',
+              null,
+              this.props.weeksOnList
+            )
+          );
+        }
+      }]);
+    
+      return BestSeller;
+    }(_react3.default.Component));
+    
+    BestSeller.propTypes = {};
+    
+    },{"/home/ccuser/.babelscripts/react-transform-component-exports":1,"react":undefined}],3:[function(require,module,exports){
+    'use strict';
+    
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.BookList = undefined;
+    
+    var _react2 = require('react');
+    
+    var _react3 = _interopRequireDefault(_react2);
+    
+    var _reactTransformComponentExports = require('/home/ccuser/.babelscripts/react-transform-component-exports');
+    
+    var _reactTransformComponentExports2 = _interopRequireDefault(_reactTransformComponentExports);
+    
+    var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+    
+    var _reactDom = require('react-dom');
+    
+    var _reactDom2 = _interopRequireDefault(_reactDom);
+    
+    var _BestSeller = require('./BestSeller');
+    
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    
+    function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+    
+    function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+    
+    var _components = {
+      BookList: {
+        displayName: 'BookList'
+      }
+    };
+    
+    var _homeCcuserBabelscriptsReactTransformComponentExports2 = (0, _reactTransformComponentExports2.default)({
+      filename: '/home/ccuser/workspace/react-102-proptypes-proptypes-apply/BookList.js',
+      components: _components,
+      locals: [module],
+      imports: [_react3.default]
+    });
+    
+    function _wrapComponent(id) {
+      return function (Component) {
+        return _homeCcuserBabelscriptsReactTransformComponentExports2(Component, id);
+      };
+    }
+    
+    var BookList = exports.BookList = _wrapComponent('BookList')(function (_React$Component) {
+      _inherits(BookList, _React$Component);
+    
+      function BookList() {
+        _classCallCheck(this, BookList);
+    
+        return _possibleConstructorReturn(this, (BookList.__proto__ || Object.getPrototypeOf(BookList)).apply(this, arguments));
+      }
+    
+      _createClass(BookList, [{
+        key: 'render',
+        value: function render() {
+          return _react3.default.createElement(
+            'div',
+            null,
+            _react3.default.createElement(
+              'h1',
+              null,
+              'Best Sellers'
+            ),
+            _react3.default.createElement(
+              'div',
+              null,
+              _react3.default.createElement(
+                'ol',
+                null,
+                _react3.default.createElement(_BestSeller.BestSeller, {
+                  title: 'Glory and War Stuff for Dads',
+                  author: 'Sir Eldrich Van Hoorsgaard',
+                  weeksOnList: 10 }),
+                _react3.default.createElement(_BestSeller.BestSeller, {
+                  title: 'The Crime Criminals!',
+                  author: 'Brenda Sqrentun',
+                  weeksOnList: 2 }),
+                _react3.default.createElement(_BestSeller.BestSeller, {
+                  title: 'Subprime Lending For Punk Rockers',
+                  author: 'Malcolm McLaren',
+                  weeksOnList: 600 })
+              )
+            )
+          );
+        }
+      }]);
+    
+      return BookList;
+    }(_react3.default.Component));
+    
+    },{"./BestSeller":2,"/home/ccuser/.babelscripts/react-transform-component-exports":1,"react":undefined,"react-dom":undefined}]},{},[3]);
+    
